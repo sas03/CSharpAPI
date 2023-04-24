b7bdd10863ee7b524a32c036656b6475dad3a192
stephsumo <stephane.sumo@nexgen-si.de>
2023-04-24 20:11:30 +0200

    csharpapi

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using CSKarteAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSKarteAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        /*public IActionResult Index()
+        {
+            return View();
+        }*/
+
+        public async Task<IActionResult> Index()
+        {
+            // Objet RestRequest utilisé dans les RESTful APIs pour récupérer des informations à partir du endpoint salesOrder 
+            RestRequest request = new RestRequest("/salesOrder");
+            // Ajout d'un AuthenticationToken dans le header de l'objet RestRequest
+            request.AddHeader("AuthenticationToken", "08f67197-1bba-47e9-a892-38139da5adcf");
+            // Objet RestClient(RESTful API client) qui prend l'URL racine de l'API en paramètre, pour envoyer une requete au serveur 
+            RestClient client = new RestClient("https://nexgen.weclapp.com/webapp/api/v1");
+
+            //IRestResponse result = await client.ExecuteAsync(request);
+            var response = await client.ExecuteAsync(request);
+
+            // objet avec comme clé result et valeur un array d'objets
+            // Deserialize the response(json string) into an object and retrieve the data from the response
+            WeclappResult<WeclappAuftrag> data = JsonConvert.DeserializeObject<WeclappResult<WeclappAuftrag>>(response.Content); 
+
+            // pass the data(data.result = array of objects with the type WeclappAuftrag) to the view
+            return View(data.result); 
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/WeclappAuftrag.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSKarteAPI.Models
+{
+    //Définition d'une classe WeclappResult, qui peut etre utilisé avec un type de donnée random(T)
+    public class WeclappResult<T>
+    {
+        //La propriété result(un tableau de type random(T)) a des accesseurs get et set permettant de la lire et l'écrire à l'extérieur de la classe
+        public T[] result { get; set; }
+    }
+
+    //La classe WeclappAuftragen hérite des propriétés et méthodes de la classe WeclappResult, qui utilise des données de type WeclappAuftrag
+    public class WeclappAuftragen : WeclappResult<WeclappAuftrag> { }
+
+    //La classe WeclappAddresses hérite des propriétés et méthodes de la classe WeclappResult, qui utilise des données de type WeclappAddress
+    public class WeclappAddresses : WeclappResult<WeclappAddress> { }
+
+    //Définition de la classe WeclappAddress avec des propriétés
+    public class WeclappAddress
+    {
+        // Attributs avec accesseurs get et set
+        public string company { get; set; }
+        public string company2 { get; set; }
+        public string city { get; set; }
+        public string street1 { get; set; }
+        public string zipcode { get; set; }
+        // Constructeur
+        public WeclappAddress() { }
+    }
+
+    //Définition de la classe WeclappAuftrag
+    public class WeclappAuftrag
+    {
+        // Attributs avec accesseurs get et set
+        public string customerNumber { get; set; }
+        public string netAmount { get; set; }
+        public string createdDate { get; set; }
+
+        // Attribut deliveryAddress(de type WeclappAddress) avec accesseurs get et set
+        public WeclappAddress deliveryAddress { get; set; }
+
+        // Constructeur
+        public WeclappAuftrag() { }
+    }
+}
